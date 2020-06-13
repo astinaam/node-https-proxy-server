5ef94898a56e357b0eeee4c9c0016adcd8b56d0d
astinaam <abdulahalmahmud@gmail.com>
2020-06-13 18:41:47 +0600

    Add all  changes at once

---
+++ README.md
@@ -0,0 +1,7 @@
+Creates a simple https proxy server. Target http server location can be set from  proxy-settings.json file.
+
+### Running server
+```
+npm install
+npm start
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+var httpProxy = require("http-proxy");
+var fs = require("fs");
+
+// read config from file
+var config = require("./proxy-settings.json");
+
+var serverKey = fs.readFileSync("./server.key");
+var servercert = fs.readFileSync("./server.cert");
+
+// create proxy server
+httpProxy
+  .createServer({
+    target: {
+      host: config.httpTarget,
+      port: config.httpPort,
+    },
+    ssl: {
+      key: sk,
+      cert: sc,
+    },
+  })
+  .listen(config.proxyPort);
+
+console.log(
+  `Listening https on port ${config.proxyPort} (proxy server to http://${config.httpTarget}:${config.httpPort})...`
+);
